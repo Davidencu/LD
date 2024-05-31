fcaf8c0f170d981e5386f65ea0875ebe455304ef
Davidencu <150547056+Davidencu@users.noreply.github.com>
2024-05-31 18:58:31 +0300

    Update prj.tcl

--- dlatch/prj.tcl
+++ dlatch/prj.tcl
@@ -4,10 +4,10 @@ set_global_assignment -name FAMILY MAX10
 set_global_assignment -name DEVICE 10M50DAF484C7G 
 
 set_global_assignment -name BDF_FILE example1.bdf
-set_global_assignment -name VERILOG_FILE ../verilog/dlatch.v
+set_global_assignment -name VERILOG_FILE ../verilog/Dlatch.v
 set_global_assignment -name SDC_FILE example1.sdc
 
-set_global_assignment -name TOP_LEVEL_ENTITY dlatch
+set_global_assignment -name TOP_LEVEL_ENTITY Dlatch
 set_location_assignment -to clk PIN_AH10
 
 set_location_assignment PIN_C10 -to d
